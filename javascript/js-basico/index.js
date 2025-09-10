1e417e77536d4401e687941482963b5520990fc7
Eliud-Garcia <jhonniereliudgarciacapera@gmail.com>
2025-09-10 11:46:45 -0500

    js-basico

---
+++ javascript/js-basico/index.js
@@ -0,0 +1,43 @@
+
+//variables
+let a = "Hola";
+let b = 10;
+let c = 43.831;
+
+//salida por consola
+console.log(a);
+console.log(b);
+console.log(c);
+
+//entrada
+//let edad = prompt('Ingrese su edad');
+
+
+// arreglos
+array = ['hola', 10, 98, false, a];
+array.push(0);
+
+
+//for
+for(let i = 0; i < 10; i++){
+    console.log(i);
+}
+array.forEach(element => {
+    console.log(element);
+});
+
+
+//objetos
+
+objeto = {id:34, nombre:"Eliud"};
+console.log(objeto);
+
+//funciones
+
+function saludar(nombre){
+    alert('Hola ' + nombre)
+}
+
+const funcionFlecha = ()=>{console.log('usando funcion flecha')};
+
+
